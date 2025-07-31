5fe2db09d0e1f659431470a19f172a77b6f0826b
lianozzzz <lianips2@gmail.com>
2025-07-31 22:23:31 +0700

    perubahan cyber

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -57,7 +57,13 @@
                      <div class="banner-right-con wow slideInRight">
                         <img src="public/template/landing/assets/image/slider-item-img1.png" alt="banner image" class="img-fluid">
                      </div>
-                  </div>
+                  </div><div class="col-lg-5 col-md-5 col-sm-12">
+                           <div class="banner-right-con wow slideInRight">
+                              <figure class="mb-0">
+                                 {{-- <img src="assets/image/banner-right-img.png" alt="banner-right-img"> --}}
+                              </figure>
+                           </div>
+                        </div>
                </div>
             </div>
          </div>
