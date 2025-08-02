37a8ef0c3deed39405ed60cc97fdaad57912e432
lianozzzz <lianips2@gmail.com>
2025-08-02 23:54:32 +0700

    perubahan judull

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -15,7 +15,7 @@
     <link rel="shortcut icon" type="image/png" href="images/favicon.png" />
 
     <link href="public/template/assets/vendor/jquery-nice-select/css/nice-select.css" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('template/assets') }}/vendor/nouislider/nouislider.min.css">
+    <link rel="stylesheet" href="public/template/assets/vendor/nouislider/nouislider.min.css">
     <!-- Style css -->
     <link href="public/template/assets/css/style.css" rel="stylesheet">
     <link href="public/template/assets/vendor/datatables/css/jquery.dataTables.min.css" rel="stylesheet">
