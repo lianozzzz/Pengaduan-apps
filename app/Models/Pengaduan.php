48fdd25550bdb0b1f00ae76f1253f772cad003d4
lianozzzz <lianips2@gmail.com>
2025-08-13 04:50:06 +0700

    pol

--- app/Models/Pengaduan.php
+++ app/Models/Pengaduan.php
@@ -24,9 +24,10 @@ class Pengaduan extends Model
 
     // Model Pengaduan.php
     public function user()
-    {
-        return $this->belongsTo(User::class, 'user_id', 'user_id');
-    }
+{
+    return $this->belongsTo(User::class, 'user_id', 'id');
+}
+
 
 
     public function foto()
