a5bb40f8cbcbbcbd65e7bea8783231cb57867616
lianozzzz <lianips2@gmail.com>
2025-08-13 02:41:00 +0700

    yubah

--- database/migrations/2025_08_13_024020_remove_auto_increment_from_id_on_users_table.php
+++ database/migrations/2025_08_13_024020_remove_auto_increment_from_id_on_users_table.php
@@ -6,13 +6,11 @@ use Illuminate\Support\Facades\DB;
 return new class extends Migration {
     public function up(): void
     {
-        // Hapus AUTO_INCREMENT dari kolom id
         DB::statement('ALTER TABLE users MODIFY id BIGINT(20) UNSIGNED NOT NULL');
     }
 
     public function down(): void
     {
-        // Balikkan ke AUTO_INCREMENT
         DB::statement('ALTER TABLE users MODIFY id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT');
     }
 };
