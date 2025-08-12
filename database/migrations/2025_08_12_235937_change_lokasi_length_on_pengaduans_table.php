f764b05043823e4cdc55e2b86d533aa171321497
lianozzzz <lianips2@gmail.com>
2025-08-13 00:00:22 +0700

    perubahan aksol

--- database/migrations/2025_08_12_235937_change_lokasi_length_on_pengaduans_table.php
+++ database/migrations/2025_08_12_235937_change_lokasi_length_on_pengaduans_table.php
@@ -3,20 +3,30 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 return new class extends Migration {
     public function up(): void
     {
+        // Matikan sementara pengecekan foreign key
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
         Schema::table('pengaduans', function (Blueprint $table) {
             $table->string('lokasi', 100)->change();
         });
+
+        // Nyalakan lagi pengecekan foreign key
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 
     public function down(): void
     {
+        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+
         Schema::table('pengaduans', function (Blueprint $table) {
             $table->string('lokasi', 50)->change();
         });
+
+        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 };
-
